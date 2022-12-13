f36d29df06b4b4e4adbc3222029adae4e65f419c
Antonin <antonin.guyon861@gmail.com>
2022-12-13 13:55:30 +0100

    go

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aguyon <aguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 21:46:27 by aguyon            #+#    #+#             */
-/*   Updated: 2022/12/12 23:32:02 by aguyon           ###   ########.fr       */
+/*   Updated: 2022/12/13 12:04:00 by aguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,7 @@
 #include "Libft/libft.h"
 #include <stdarg.h>
 
-enum e_index_conversions{c, s};
 
-typedef int (*putchar_ptr)(char, int fd);
-typedef int (*putstr_ptr)(char *, int fd);
-
-int	ft_printf(void);
+int	ft_printf(const char *format, ...);
 
 #endif /* FT_PRINTF_H */
